e88e91737445b0149bb725605bf594a0ca782540
MariaPalma2 <146418038+MariaPalma2@users.noreply.github.com>
2025-03-21 13:26:26 +0100

    Delete modulo_biblioteca directory

--- modulo_biblioteca/models/library_author.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class LibraryAuthor(models.Model):
-    _name = 'library.author'
-    _description = 'Library Author'
-
-    name = fields.Char(string="Name", required=True)
-    biography = fields.Text(string="Biography")
--- modulo_biblioteca/report/_init_.py
+++
@@ -1,5 +0,0 @@
-
-from . import report_active_loans
-from . import report_books_available
-from . import report_loan_history
-from . import report_registered_users
